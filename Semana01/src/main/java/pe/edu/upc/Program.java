426accc282b443ed2c171090d4ad57b8569c1cdc
Luis Alfredo Porras <luisporras2514@gmail.com>
2022-03-25 20:39:33 -0500

    Creación de clase y objeto estudiante

---
+++ Semana01/src/main/java/pe/edu/upc/Program.java
@@ -0,0 +1,13 @@
+package pe.edu.upc;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Student student = new Student();
+		student.setFirstName("Luis Alfredo Porras Tarifeño");
+		System.out.println(student.getFirstName());
+		
+	}
+
+}
